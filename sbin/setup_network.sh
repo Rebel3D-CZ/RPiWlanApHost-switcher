999b9d03c039a0d7af0dd7a76cd495d3e823b623
camel1cz <kamil.srot@nlogy.com>
2022-02-21 17:54:30 +0100

    Fix folder ownership

--- sbin/setup_network.sh
+++ sbin/setup_network.sh
@@ -6,3 +6,6 @@
 # setup wifi profiles
 HNAME=`hostname`
 /opt/rebelove.org/sbin/setup_wlan_and_AP_modes.sh -s stationSSID -p 'rebelove.org' -a $HNAME -r 'rebelove.org'
+
+# setup connect access rights for /opt/rebelove.org/*
+chown pi /opt/rebelove.org/var
