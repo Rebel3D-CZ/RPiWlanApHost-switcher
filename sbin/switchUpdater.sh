2439af64ad8343eef3410df26c6f8e50577c1358
Rebel3D-CZ <69604290+Rebel3D-CZ@users.noreply.github.com>
2022-02-25 17:48:56 +0100

    Update switchUpdater.sh

--- sbin/switchUpdater.sh
+++ sbin/switchUpdater.sh
@@ -4,5 +4,5 @@ cd /home/pi/RPiWlanApHost-switcher
 git pull https://github.com/Rebel3D-CZ/RPiWlanApHost-switcher.git
 
 sudo cp -r -u /home/pi/RPiWlanApHost-switcher/* /opt/rebelove.org/
-systemctl daemon-reload
-systemctl restart r3d-daemon.service
+sudo systemctl daemon-reload
+sudo systemctl restart r3d-daemon.service
